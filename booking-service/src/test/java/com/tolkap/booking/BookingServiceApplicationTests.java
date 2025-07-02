a3c1574b858925b8cb70358118fff29c6fb9c3f8
Agarshan Senthamipalan <agarshan.senthamipalan@infor.com>
2025-07-02 21:09:14 +0530

    Initial commit auth-service and court-service

---
+++ booking-service/src/test/java/com/tolkap/booking/BookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tolkap.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
